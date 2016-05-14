fd7df6885d66742f9ef849be24a731510ed83ab0
Ju Liu <ju.liu@welaika.com>
2016-05-14 21:38:51 +0100

    Refactor hello-there to reuse same generator function

--- app/components/hello-there.js
+++ app/components/hello-there.js
@@ -1,19 +1,14 @@
 import Ember from 'ember';
 import { task, timeout } from 'ember-concurrency';
 
+let HELLO_THERE_FN = function* () {
+  yield timeout(1000);
+  alert("hello there!");
+};
+
 export default Ember.Component.extend({
-  greet: task(function* () {
-    yield timeout(1000);
-    alert("hello there!");
-  }),
-
-  greetDebounced: task(function* () {
-    yield timeout(1000);
-    alert("hello there!");
-  }).restartable(),
-
-  greetDrop: task(function* () {
-    yield timeout(1000);
-    alert("hello there!");
-  }).drop()
+  greet: task(HELLO_THERE_FN),
+  greetDebounced: task(HELLO_THERE_FN).restartable(),
+  greetEnqueued: task(HELLO_THERE_FN).enqueue(),
+  greetDrop: task(HELLO_THERE_FN).drop()
 });
