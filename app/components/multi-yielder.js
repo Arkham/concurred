1eb52ab412f116285b01a137b8d35ba17e5f1abc
Ju Liu <ju.liu@welaika.com>
2016-05-14 16:14:41 +0100

    Add multi yielder component

---
+++ app/components/multi-yielder.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
+
+const { set } = Ember;
+
+export default Ember.Component.extend({
+  waitAFewSeconds: task(function* () {
+    set(this, 'status', 'Gimme one second..');
+    yield timeout(1000);
+    set(this, 'status', 'Gimme one more second..');
+    yield timeout(1000);
+    set(this, 'status', 'OK, I am done!');
+  }),
+
+  waitForPromise: task(function* () {
+    set(this, 'status', 'Waiting...');
+    let promise = timeout(1000).then(() => 12345);
+    let resolvedValue = yield promise;
+    set(this, 'status', `The value of the promise is ${resolvedValue}`);
+  }),
+
+  waitForFailingPromise: task(function* () {
+    set(this, 'status', 'Waiting...');
+
+    let promise = timeout(1000).then(() => {
+      throw 'Ayeeeeee';
+    });
+
+    try {
+      let resolvedValue = yield promise;
+    } catch(e) {
+      set(this, 'status', `The value of the error is ${e}`);
+    }
+  })
+});
